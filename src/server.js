a3d5c19e652ff5ed7aaf6662a7527bf68b6c5e3f
Josh L <joshua.levine42@gmail.com>
2025-01-27 19:01:26 -0500

    full chat functionality

--- src/server.js
+++ src/server.js
@@ -1,3 +1,35 @@
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const cors = require("cors");
+// const routes = require("./routes");
+// app.use(cors());
+
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: { origin: "*", methods: ["GET", "POST"] },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`a user connected ${socket.id}`);
+  
+//   socket.on("chat_message", (data) => {
+//     socket.broadcast.emit("receive_message", data);
+//   });
+// });
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api", routes);
+
+
+// server.listen(5001, () => {
+//   console.log("listening on 5001");
+// });
+
+// EVERYTHING ABOVE THIS LINE IS TRIAL
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
@@ -5,6 +37,7 @@ const dotenv = require("dotenv");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const eiows = require("eiows");
 
 // Load environment variables
 dotenv.config();
@@ -12,38 +45,59 @@ dotenv.config();
 // Create the app and server
 const app = express();
 const server = http.createServer(app);
-
-// Initialize Socket.IO
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
+// Initialize Socket.IO
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
+
+const io = new Server(server, {
+    cors: {
+      origin: "*", methods: ["GET", "POST"]
+    },
+  });
+
+
+// io.on("connection", (socket) => {
+//   console.log(`a user connected ${socket.id}`);
+  
+//   socket.on("chat_message", (data) => {
+//     socket.broadcast.emit("receive_message", data);
+//   });
+// });
+// server.listen(5001, () => {
+//   console.log("listening on 5001");
+// });
 // Socket.IO logic
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log(`a user connected ${socket.id}`);
 
-  socket.on("join", (username) => {
-    io.emit("user-joined", username);
-  });
+//   socket.on("join", (username) => {
+//     io.emit("user-joined", username);
+//   });
+// // original here
+//   socket.on("chat-message", (data) => {
+//     socket.broadcast.emit("chat-message", `${data.username}: ${data.message}`);
+//   });
 
   socket.on("chat-message", (data) => {
-    socket.broadcast.emit("chat-message", `${data.username}: ${data.message}`);
+    io.emit("chat-message", data);
+    // io.emit("chat-message", `${data.username}: ${data.message}`);
+    // console.log(data)
   });
+//   socket.on("leave", (username) => {
+//     io.emit("user-left", username);
+//   });
 
-  socket.on("leave", (username) => {
-    io.emit("user-left", username);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
+//   socket.on("disconnect", (reason) => {
+//     console.log(`A user disconnected: ${socket.id}`, reason);
+//   });
 });
 
 // Connect to MongoDB and start the server
@@ -58,3 +112,9 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
+
+
+// // This is added be prepped to remove
+// server.listen(5001, () => {
+//   console.log("listening on 5001");
+// });
